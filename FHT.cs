a87458eabffca1b20d35c2851d2eadbc176ac7c1
alex_basic <alex_basic@mail.ru>
2014-09-01 10:09:43 +0600

    Спектральный анализ алгоритмом FHT

---
+++ FHT.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FHT
+{
+    class FHT
+    {
+        public static double[] CalcForward(int[] input)
+        {
+            int N = input.Length;
+            double[] result = new double[N];
+            double pi2 = (Math.PI * 2) / N;
+            for (int k = 0; k < N; k++)
+            {
+                double pi2k = pi2 * k;
+                for (int n = 0; n < N; n++)
+                {
+                    double s = (pi2k * n) ;
+                    result[k] += (Math.Sin(s)+Math.Cos(s)) * input[n];
+                }
+            }
+            return result;
+        }
+
+        public static double[] CalcBackward(double[] input)
+        {
+            int N = input.Length;
+            double[] result = new double[N];
+            double pi2 = (Math.PI * 2) / N;
+            for (int k = 0; k < N; k++)
+            {
+                double pi2k = pi2 * k;
+                for (int n = 0; n < N; n++)
+                {
+                    double s = (pi2k * n);
+                    result[k] += (Math.Sin(s) + Math.Cos(s)) * input[n];
+                }
+                result[k] = result[k] / N;
+            }
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using FFT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FHT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int period = 1024;
+            int[] signal = Generator.GetSin(period, 15, 128);
+            int[] signal2 = Generator.GetSin(period, 1.5, 0);
+            signal = Generator.CombineSignal(signal, signal2);
+            //signal = GetMeandr(period);
+            double[] output = FHT.CalcForward(signal);
+            var pl = new PlotWindow("signal", signal);
+            var p2 = new PlotWindow("FHT", Helper.DoubleToInt(output, 0.03, true), true);
+            var p3 = new PlotWindow("sin", Helper.DoubleToInt(FHT.CalcBackward(output)));
+            Console.WriteLine("Waiting for exit...");
+            Console.ReadKey();
+        }
+    }
+}
